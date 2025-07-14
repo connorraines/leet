c705b248b98ce00dc66603c4dc23831cbab4b207
connorraines <connorlraines@gmail.com>
2025-07-14 09:07:25 -0500

    feat: Completed NNext Challenge

---
+++ singleNumber/singleNumber.py
@@ -0,0 +1,15 @@
+class Solution(object):
+    def singleNumber(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        num_dict = {}
+        for num in nums:
+            if num in num_dict:
+                num_dict[num] += 1
+            else:
+                num_dict[num] = 1
+        for key, value in num_dict.items():
+            if value == 1:
+                return key
