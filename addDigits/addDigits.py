12bc6580527bd393e17ff454390cd900c4c080bb
connorraines <connorlraines@gmail.com>
2025-07-15 08:27:35 -0500

    feat: Completed Next Challenge

---
+++ addDigits/addDigits.py
@@ -0,0 +1,9 @@
+class Solution(object):
+    def addDigits(self, num):
+        """
+        :type num: int
+        :rtype: int
+        """
+        while num >= 10:
+            num = sum(int(digit) for digit in str(num))
+        return num
